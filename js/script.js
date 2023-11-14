9c80d283932e991722c4d704dd53b2566f55499c
Alex <example@gmail.com>
2023-11-14 15:15:57 +0400

    WoRkJSDZ14.11.2023

--- js/script.js
+++ js/script.js
@@ -202,3 +202,101 @@
 //     }
 //     console.log(i);
 // }
+
+// fot (let i = 1, i < 3, i++) {
+//    console.log(i);
+//    fot (let j = 1, j < 3, j++)
+//    console.log(j);
+// }
+
+//Дополнительный урок №22 цикл в цикле и метки
+// let result = '';
+// const length = 10;
+
+// for (let i = 1; i < length; i++){
+
+//    for (let j = 0; j < i; j++){
+//       result += "*";
+//    }
+
+//    result += '\n';
+// }
+
+// console.log(result);
+
+
+// let star = '*';
+
+// let length = 6;
+
+// let result = '';
+
+// for (let i = 1; i < length; i++) {
+
+//     console.log(result += star);
+
+// }
+
+// let result = '';
+
+// for (let i = 1; i < 6; i++) {
+
+//     console.log(result += '*');
+
+// }
+
+// first: for (let i = 0; i < 3; i++) {
+//    console.log(`first level: ${i}`);
+//    for (let j = 0; j < 3; j++) {
+//       console.log(`Seconsd level: ${j}`);
+//       for (let k = 0; k < 3; k++) {
+//          if (k === 2) continue first;
+//          console.log(`Third level: ${k}`);
+//       }
+//    } 
+// }
+
+//задачи
+
+//1
+// for (let i = 5; i < 11; i++) {
+//    console.log(i);
+// }
+// //2
+// for (let i = 20; i > 10; i--) {
+//    if ( i === 12){
+//       break
+//    }
+//    console.log(i);
+// }
+// //3
+// for (let i = 2; i <=10 ; i+=2) {
+  
+//    console.log(i);
+// }
+
+// //4
+// let num = 2;
+// while (num <= 16) {
+//     if(num % 2 === 1) {
+//     console.log(num);
+//     num++;
+//     }else {
+//     num++;
+//     }
+// }
+// //5
+// const arrayOfNumbers = [];
+// for (let i = 5; i < 11; i++) {
+// arrayOfNumbers[i - 5] = i
+// }
+// console.log(arrayOfNumbers);
+
+const arrayOfNumbers = [];
+arrayOfNumbers[0] = 5
+arrayOfNumbers[1] = 6
+arrayOfNumbers[2] = 7
+arrayOfNumbers[3] = 8
+
+
+console.log(arrayOfNumbers);
