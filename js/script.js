e658cb4d809da2a64d88bd667fd230ae09a7527f
Alex <example@gmail.com>
2023-11-16 10:36:49 +0400

    homeWorkJs16.11

--- js/script.js
+++ js/script.js
@@ -258,19 +258,3 @@
 
 //
 
-// for (let i = 5; i < 11; i++) {
-//     console.log(i);
-// }
-
-// for (let i = 20; i > 10; i--) {
-//     if(i === 13){
-//         break
-//     }
-//     console.log(i)
-// }
-
-for (let i = 2; i <=10 ; i++) {
-    if (i % 2 === 0)  { 
-      console.log(i); 
-    }
-}
